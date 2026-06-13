126ea6b839faa8524a032753609adda6a7edcbf9
Astria Rizka <astria.rizka.tif423@polban.ac.id>
2026-06-13 15:04:02 +0700

    add hooks and driver setup

--- src/main/java/hooks/Hooks.java
+++ src/main/java/hooks/Hooks.java
@@ -1,4 +1,20 @@
 package hooks;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import utils.DriverSetup;
+
 public class Hooks {
+
+    @Before
+    public void setUp() {
+
+        DriverSetup.getDriver();
+    }
+
+    @After
+    public void tearDown() {
+
+        DriverSetup.driver.quit();
+    }
 }
--- src/main/java/utils/DriverSetup.java
+++ src/main/java/utils/DriverSetup.java
@@ -1,4 +1,24 @@
 package utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverSetup {
+
+    public static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+
+            WebDriverManager.chromedriver().setup();
+
+            driver = new ChromeDriver();
+
+            driver.manage().window().maximize();
+        }
+
+        return driver;
+    }
 }
